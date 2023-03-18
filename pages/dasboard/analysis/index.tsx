c25426de82ffe578f98091fd20e556fbb280d857
mosihcroctiv <chizzyyoung10@gmail.com>
2023-03-18 10:03:01 +0100

    done with sidebar

---
+++ pages/dasboard/analysis/index.tsx
@@ -0,0 +1,14 @@
+import Sidebar from '../../../components/dashboard/Sidebar'
+import Navbar from '../../../components/NavBar'
+
+const AnalysisIndex = ()=>{
+
+    return(
+        <>
+        <Navbar/>
+        <Sidebar/>
+        </>
+    )
+}
+
+export default AnalysisIndex
